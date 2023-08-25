ca6b0ca31b51cd9bea698c9171201b87e1c3b874
Rafael Torales de Souza <rafaeltoraless@hotmail.com>
2023-08-25 18:52:27 -0300

    Mais funções e uma calculadora

---
+++ JavaScript/Aula 06/script.js
@@ -0,0 +1,20 @@
+// Declarativa
+function funcao() {
+    console.log("Olá");
+}
+
+// Expressões de funções
+// Com nomeação
+var funcao1 = function funcao1() {
+    console.log("Olá");
+};
+
+// Sem nomeação
+var funcao2 = function () {
+    console.log("Olá");
+};
+
+// Arrow
+var funcao3 = () => {
+    console.log("Olá");
+};
