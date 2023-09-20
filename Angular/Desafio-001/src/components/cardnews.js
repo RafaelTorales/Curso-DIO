c1bb7683d7d4dfcd9ddd013a5ebe0976af141e2f
Rafael Torales de Souza <rafaeltoraless@hotmail.com>
2023-09-20 19:51:09 -0300

    Setando Propriedades de classes

--- Angular/Desafio-001/src/components/cardnews.js
+++ Angular/Desafio-001/src/components/cardnews.js
@@ -2,16 +2,27 @@ class Cardnews extends HTMLElement {
     constructor() {
         super();
         const shadow = this.attachShadow({ mode: "open" });
-        shadow.innerHTML = "<h1>Hello</h1>";
+        shadow.appendChild(this.build());
+        shadow.appendChild(this.styles());
     }
 
-    build(){
+    build() {
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class", "card");
 
+        const cardLeft = document.createElement("div");
+        cardLeft.setAttribute("class", "card_left");
+
+        const cardRight = document.createElement("div");
+        cardRight.setAttribute("class", "card_right");
+
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRight);
+
+        return componentRoot;
     }
 
-    styles() {
-        
-    }
+    styles() {}
 }
 
 customElements.define("card-news", Cardnews);
