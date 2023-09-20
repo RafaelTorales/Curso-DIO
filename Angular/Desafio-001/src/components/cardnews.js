2ed658ce187bea553a921f5fcf488077750447f7
Rafael Torales de Souza <rafaeltoraless@hotmail.com>
2023-09-20 20:21:25 -0300

    Construindo as Props do card news

--- Angular/Desafio-001/src/components/cardnews.js
+++ Angular/Desafio-001/src/components/cardnews.js
@@ -11,10 +11,21 @@ class Cardnews extends HTMLElement {
         componentRoot.setAttribute("class", "card");
 
         const cardLeft = document.createElement("div");
+        const autor = document.createElement("span");
+        const linkTitle = document.createElement("a");
+        const newsContent = document.createElement("p");
         cardLeft.setAttribute("class", "card_left");
+        cardLeft.appendChild(autor);
+        cardLeft.appendChild(linkTitle);
+        cardLeft.appendChild(newsContent);
+        autor.textContent = "By " + (this.getAttribute("autor") || "Anonymous");
+        linkTitle.textContent = this.getAttribute("title")
+        newsContent.textContent = this.getAttribute("content")
 
         const cardRight = document.createElement("div");
+        const newsImage = document.createElement("img");
         cardRight.setAttribute("class", "card_right");
+        cardRight.appendChild(newsImage);
 
         componentRoot.appendChild(cardLeft);
         componentRoot.appendChild(cardRight);
