30be8877fbd8602ade878440f9f4990c25cb4dea
Rafael Torales de Souza <rafaeltoraless@hotmail.com>
2023-09-28 17:02:09 -0300

    Utilizando Generics

--- TypeScript/src/index.ts
+++ TypeScript/src/index.ts
@@ -1,9 +1,9 @@
 // Generics
-function concatArray(...itens: any[]): any[] {
+function concatArray<T>(...itens: T[]): T[] {
     return new Array().concat(...itens);
 }
 
-const numArray = concatArray([1, 5], [3]);
-const stgArray = concatArray(["Rafael", "Goku"], ["Veggeta"]);
+const numArray = concatArray<number[]>([1, 5], [3]);
+const stgArray = concatArray<string[]>(["Rafael", "Goku"], ["Veggeta"]);
 console.log(numArray);
 console.log(stgArray);
