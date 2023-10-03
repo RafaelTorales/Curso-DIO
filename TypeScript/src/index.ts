7d86c13d565752cf948ffdf4171f31a162892fad
Rafael Torales de Souza <rafaeltoraless@hotmail.com>
2023-10-03 13:33:31 -0300

    Atrribute Decorator

--- TypeScript/src/index.ts
+++ TypeScript/src/index.ts
@@ -1,12 +1,50 @@
 // Decorators
+function ExibirNome(target: any) {
+    console.log(target);
+}
+
+@ExibirNome
+class Funcionario {}
+@ExibirNome
+class Quincas {}
+
+// Class decorator
 function apiVersion(version: string) {
-    return (target) => {
-        Object.assign(target.prototype, { __version: version });
+    return (targett: any) => {
+        Object.assign(targett.prototype, { __version: version });
     };
 }
 
-@apiVersion("1.10")
-class Api {}
+// Attribute decorator
 
-const api = new Api();
-console.log(api.__version);
+function minLenght(lenght: number) {
+    return (target: any, key: string) => {
+        let _value = target[key];
+
+        const getter = () => _value;
+        const setter = (value: string) => {
+            if (value.length < lenght) {
+                throw new Error(`Tamanho menor do que ${lenght}`);
+            } else {
+                _value = value;
+            }
+        };
+
+        Object.defineProperty(target, key, {
+            get: getter,
+            set: setter,
+        });
+    };
+}
+
+class Api {
+    @minLenght(3)
+    name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+}
+
+const api = new Api("produtros");
+console.log(api.name);
